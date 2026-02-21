3f91d7ef0bfbe376e11f77caf0b644491ace309e
deltaplanoo <pietro.ciani01@gmail.com>
2026-02-21 18:55:59 +0100

    feat: ddgs for web queries

---
+++ client.py
@@ -0,0 +1,29 @@
+import asyncio
+from fastmcp import Client
+from rich import print
+
+server_url = "http://localhost:8000/sse"
+
+async def main():
+    print("Connessione ai server in corso...")
+    
+    client = Client(server_url)
+    
+    async with client:
+        print("[green] Connesso \n")
+        
+        mcp_tools_list = await client.list_tools()
+        print(mcp_tools_list)
+        print("-" * 50)
+        
+        user_query = "Conviene aprire un ristorante in via dei Calzaiuoli 50 a Firenze?"
+        
+        try:
+            risultato = await client.call_tool("web_search", arguments={"query": user_query})
+            print(f"[green]Risposta dal Server:[/green] {risultato} \n")
+
+        except Exception as e:
+            print(f"[red]Errore Server:[/red] {e}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from fastmcp import FastMCP
-
+from duckduckgo_search import DDGS
 
 mcp = FastMCP("Server")
 
-
 @mcp.tool()
-def saluta_da_server(args: str = "Argomento Server") -> str:
-	"""Restituisce un saluto dal Server"""
+def web_search(query: str) -> str:
+    """
+    Searches the web for information relevant to the decision.
+    """
+    print(f"[LOG SERVER] Searching key factors for decision: {query}")
+    
+    with DDGS() as ddgs:
+        # top 5 results
+        extended_query = f"What factors should I consider when making this decision: {query}"
+        results = ddgs.text(extended_query, max_results=5)
+        
+        if not results:
+            return "This query returned no results."
 
-	msg = f"Ciao, Il server è acceso!, questi sono gli argomenti: {args}"
-	print(f"[LOG SERVER] Eseguito tool con args: {args}")
-	
-	return msg
-
+        formatted_output = "Web search results:\n"
+        for i, r in enumerate(results, 1):
+            formatted_output += f"{i}. {r['title']}: {r['body']}\n"
+            
+        return formatted_output
 
 if __name__ == "__main__":
-	mcp.run(transport="sse", port=8000)
+    mcp.run(transport="sse", port=8000)
